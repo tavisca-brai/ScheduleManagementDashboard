5a5bfc0b7e76b78a93b6105d326136e376c89484
Pahwa <opahwa@internal.tavisca.com>
2019-10-10 10:16:29 +0530

    added calender validations

--- angularjs.js
+++ angularjs.js
@@ -2,24 +2,49 @@ var app = angular.module('myApp', []);
 app.controller('customersCtrl', function($scope, $http) {
 
     $scope.listOfOptions = ['SC'];
-    $scope.calculatedValue="";
+    $scope.calculatedValue = "";
 
-  	$scope.selectedItemChanged = function(){
-    calculatedValue = String($scope.selectedItem);
-    document.getElementsByClassName('filters')[0].style.display='block';
+    $scope.selectedItemChanged = function() {
+        calculatedValue = String($scope.selectedItem);
+        document.getElementsByClassName('filters')[0].style.display = 'block';
     }
-    
-    $scope.generateReport = function(){
-        switch(calculatedValue){
+
+    $scope.generateReport = function() {
+        switch (calculatedValue) {
             case 'SC':
                 $http.get("https://reqres.in/api/users?page=1")
-                .then(function (response) {$scope.names = response.data;});
+                    .then(function(response) { $scope.names = response.data; });
                 console.log($scope.names);
-                document.getElementsByClassName('logs')[0].style.display='block';
+                document.getElementsByClassName('logs')[0].style.display = 'block';
                 break;
             default:
                 console.log('notfound');
         }
-    }   
+    }
 
 });
+var setMaxDate = function(id) {
+    if (document.getElementById("toDate").value != "") {
+        var x = document.getElementById("toDate").value;
+        document.getElementById(id).setAttribute("max", x);
+    } else {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd
+        }
+        if (mm < 10) {
+            mm = '0' + mm
+        }
+        today = yyyy + '-' + mm + '-' + dd;
+        document.getElementById(id).setAttribute("max", today);
+    }
+}
+
+function setMinDate() {
+
+    var minDate = document.getElementById("fromDate").value;
+    document.getElementById("toDate").setAttribute("min", minDate);
+}
